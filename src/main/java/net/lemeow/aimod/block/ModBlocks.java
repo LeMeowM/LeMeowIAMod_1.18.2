b14e9b63755265e03e61b0f84cf32ddd0016f3bf
LeMeowM <lemeowmew@gmail.com>
2022-07-16 00:50:31 +0800

    Updated Void Quartz Crystal drop rate
    Took 3 minutes

--- src/main/java/net/lemeow/aimod/block/ModBlocks.java
+++ src/main/java/net/lemeow/aimod/block/ModBlocks.java
@@ -6,11 +6,13 @@ import net.lemeow.aimod.AIMod;
 import net.lemeow.aimod.block.custom.InfusionTableBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
@@ -18,18 +20,27 @@ public class ModBlocks {
     // Void Quartz Blocks Inputs
 
     public static final Block VOID_QUARTZ_ORE = registerBlock("void_quartz_ore",
-            new Block(FabricBlockSettings.of(Material.STONE).requiresTool().strength(50f, 1200f)),
-            ItemGroup.BUILDING_BLOCKS);
+            new OreBlock(FabricBlockSettings.of(Material.STONE).requiresTool().strength(50f, 1200f)),
+            ItemGroup.BUILDING_BLOCKS,
+            UniformIntProvider.create(5, 10));
+
 
     public static final Block VOID_QUARTZ_BLOCK = registerBlock("void_quartz_block",
             new Block(FabricBlockSettings.of(Material.METAL).strength(50f, 1200f).requiresTool()),
-            ItemGroup.MISC);
+            ItemGroup.MISC, UniformIntProvider.create(5, 10));
 
     public static final Block INFUSION_TABLE = registerVoidBlock("infusion_table",
             new InfusionTableBlock(FabricBlockSettings.of(Material.STONE).strength(50f,1200f).requiresTool()),
             ItemGroup.BUILDING_BLOCKS);
 
-    public static Block registerBlock(String name, Block block, ItemGroup group){
+
+
+
+    // End Forest Blocks?
+
+
+
+    public static Block registerBlock(String name, Block block, ItemGroup group, UniformIntProvider uniformIntProvider){
         registerBlockItem(name, block, group);
         return Registry.register(Registry.BLOCK, new Identifier(AIMod.MOD_ID, name), block);
     }
